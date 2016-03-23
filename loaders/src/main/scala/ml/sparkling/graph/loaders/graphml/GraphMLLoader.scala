524643eb9ea1c067b9e02ab3c4a2d51cfa5abd77
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-03-23 09:18:13 +0100

    Travis.ci investigation

--- loaders/src/main/scala/ml/sparkling/graph/loaders/graphml/GraphMLLoader.scala
+++ loaders/src/main/scala/ml/sparkling/graph/loaders/graphml/GraphMLLoader.scala
@@ -35,43 +35,43 @@ object GraphMLLoader {
     graphDataFrame.collect()
     println("!!!!!!!!!!!!!!!!!!!!!!!!!!!! graphdata")
 
-//
-//    val nodesKeys = sqlContext
-//      .read
-//      .format("com.databricks.spark.xml")
-//      .option("rowTag", "graphml").load(path)
-//      .flatMap(r=>Try(r.getAs[mutable.WrappedArray[Row]]("key").toArray).getOrElse(Array.empty))
-//      .filter(r=>r.getAs[String]("@for")=="node")
-//nodesKeys.collect()
-//println("!!!!!!!!!!!!!!!!!!!!!!!!!!!! nodeskeys")
-//    val attrHandler=nodesKeys
-//      .map(r=>(r.getAs[String]("@id"),GraphMLAttribute(r.getAs[String]("@attr.name"),GraphMLTypes(r.getAs[String]("@attr.type"))))).collect().toMap
-//
-//    val verticesWithData = graphDataFrame.flatMap(r => r.getAs[Any]("node") match {
-//      case data: mutable.WrappedArray[Row @unchecked] => data.array
-//      case data: Row => Array(data)
-//    })
-//
-//    val verticesIndex = verticesWithData.map(r=>r.getAs[String]("@id")).zipWithUniqueId().collect().toMap
-//    val vertices: RDD[(VertexId, Map[String,Any])] = verticesWithData
-//      .map(
-//        r=>(verticesIndex(r.getAs[String]("@id")),Try(r.getAs[mutable.WrappedArray[Row]]("data")).toOption.map(
-//        _.map(r=>{
-//          val attribute=attrHandler(r.getAs[String]("@key"))
-//          (attribute.name,attribute.handler(r.getAs[String]("#VALUE")))
-//        }).toMap
-//        ).getOrElse(Map.empty))
-//      )
-//    vertices.collect()
-//    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 vertices")
-//    val edgesRows = graphDataFrame.flatMap(r => r.getAs[Any]("edge") match {
-//      case data: mutable.WrappedArray[Row @unchecked] => data.array
-//      case data: Row => Array(data)
-//    })
-//      .map(r => Edge(verticesIndex(r.getAs[String]("@source")), verticesIndex(r.getAs[String]("@target")), Map[String,Any]("id"->r.getAs[String]("@id"))))
-//    edgesRows.collect()
-//    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 edges")
-    ???
+
+    val nodesKeys = sqlContext
+      .read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", "graphml").load(path)
+      .flatMap(r=>Try(r.getAs[mutable.WrappedArray[Row]]("key").toArray).getOrElse(Array.empty))
+      .filter(r=>r.getAs[String]("@for")=="node")
+nodesKeys.collect()
+println("!!!!!!!!!!!!!!!!!!!!!!!!!!!! nodeskeys")
+    val attrHandler=nodesKeys
+      .map(r=>(r.getAs[String]("@id"),GraphMLAttribute(r.getAs[String]("@attr.name"),GraphMLTypes(r.getAs[String]("@attr.type"))))).collect().toMap
+
+    val verticesWithData = graphDataFrame.flatMap(r => r.getAs[Any]("node") match {
+      case data: mutable.WrappedArray[Row @unchecked] => data.array
+      case data: Row => Array(data)
+    })
+
+    val verticesIndex = verticesWithData.map(r=>r.getAs[String]("@id")).zipWithUniqueId().collect().toMap
+    val vertices: RDD[(VertexId, Map[String,Any])] = verticesWithData
+      .map(
+        r=>(verticesIndex(r.getAs[String]("@id")),Try(r.getAs[mutable.WrappedArray[Row]]("data")).toOption.map(
+        _.map(r=>{
+          val attribute=attrHandler(r.getAs[String]("@key"))
+          (attribute.name,attribute.handler(r.getAs[String]("#VALUE")))
+        }).toMap
+        ).getOrElse(Map.empty))
+      )
+    vertices.collect()
+    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 vertices")
+    val edgesRows = graphDataFrame.flatMap(r => r.getAs[Any]("edge") match {
+      case data: mutable.WrappedArray[Row @unchecked] => data.array
+      case data: Row => Array(data)
+    })
+      .map(r => Edge(verticesIndex(r.getAs[String]("@source")), verticesIndex(r.getAs[String]("@target")), Map[String,Any]("id"->r.getAs[String]("@id"))))
+    edgesRows.collect()
+    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 edges")
+    Graph(vertices,edgesRows)
   }
 
 
