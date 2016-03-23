9f7adabe2be97bc5a846503f4b722fdbc67ea939
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-03-23 09:26:34 +0100

    Travis.ci issue fixed

---
+++ loaders/src/main/scala/ml/sparkling/graph/loaders/graphml/GraphMLFormat.scala
@@ -0,0 +1,34 @@
+package ml.sparkling.graph.loaders.graphml
+
+/**
+ * Created by Roman Bartusiak (roman.bartusiak@pwr.edu.pl http://riomus.github.io).
+ */
+object GraphMLFormat {
+  val forAttribute: String = "@for"
+
+  val idAttribute: String = "@id"
+
+  val nameAttribute: String = "@attr.name"
+
+  val typeAttribute: String = "@attr.type"
+
+  val keyTag: String = "key"
+
+  val graphTag: String = "graph"
+
+  val graphMLTag: String = "graphml"
+
+  val nodeTag: String = "node"
+
+  val dataTag: String = "data"
+
+  val keyAttribute: String = "@key"
+
+  val edgeTag: String = "edge"
+
+  val tagValue: String = "#VALUE"
+
+  val sourceAttribute: String = "@source"
+
+  val targetAttribute: String = "@target"
+}
