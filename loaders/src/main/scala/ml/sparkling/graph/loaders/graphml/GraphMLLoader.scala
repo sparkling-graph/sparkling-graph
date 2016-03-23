173fe3728d00912a0998b16e52638fd82c82f8a1
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-03-23 09:53:19 +0100

    Travis.ci investigation

--- loaders/src/main/scala/ml/sparkling/graph/loaders/graphml/GraphMLLoader.scala
+++ loaders/src/main/scala/ml/sparkling/graph/loaders/graphml/GraphMLLoader.scala
@@ -1,25 +1,24 @@
 package ml.sparkling.graph.loaders.graphml
 
 import com.databricks.spark.xml._
+import ml.sparkling.graph.loaders.graphml.GraphMLFormat._
 import ml.sparkling.graph.loaders.graphml.GraphMLTypes.TypeHandler
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Row, SQLContext}
 
 import scala.collection.mutable
-import scala.reflect.ClassTag
 import scala.util.Try
 
 /**
  * Created by Roman Bartusiak (roman.bartusiak@pwr.edu.pl http://riomus.github.io).
  */
 object GraphMLLoader {
-  type ValuesMap=Map[String,Any]
-  val databricksXmlFormat: String = "com.databricks.spark.xml"
+  type ValuesMap = Map[String, Any]
 
-  case class GraphMLAttribute(name:String,handler:TypeHandler)
+  case class GraphMLAttribute(name: String, handler: TypeHandler)
+
 
   /**
    * Method loads single graph from graph
@@ -31,52 +30,42 @@ object GraphMLLoader {
   def loadGraphFromML(path: String)(implicit sc: SparkContext): Graph[ValuesMap, ValuesMap] = {
     val sqlContext = new SQLContext(sc)
 
-    val graphDataFrame = sqlContext.xmlFile(path,rowTag = "graph",failFast = true)
-    graphDataFrame.collect()
-    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!! graphdata")
-//    val graphMLSchema = StructType(Array(
-//      StructField("key", ArrayType(StructType(Array(
-//        StructField("@attr.name",StringType,nullable = true),
-//        StructField("@attr.type",StringType,nullable = true),
-//        StructField("@for",StringType,nullable = true),
-//          StructField("@id",StringType,nullable = true)
-//      ))), nullable = true)))
+    val graphDataFrame = sqlContext.xmlFile(path, rowTag = graphTag, failFast = true)
 
-    val nodesKeys = sqlContext.xmlFile(path,rowTag = "graphml",failFast = true)
-      .flatMap(r=>Try(r.getAs[mutable.WrappedArray[Row]]("key").toArray).getOrElse(Array.empty))
-      .filter(r=>r.getAs[String]("@for")=="node")
-nodesKeys.collect()
-println("!!!!!!!!!!!!!!!!!!!!!!!!!!!! nodeskeys")
-    val attrHandler=nodesKeys
-      .map(r=>(r.getAs[String]("@id"),GraphMLAttribute(r.getAs[String]("@attr.name"),GraphMLTypes(r.getAs[String]("@attr.type"))))).collect().toMap
+    val nodesKeys = sqlContext.xmlFile(path, rowTag = graphMLTag, failFast = true)
+      .flatMap(r => Try(r.getAs[mutable.WrappedArray[Row]](keyTag).toArray).getOrElse(Array.empty))
+      .filter(r => r.getAs[String](forAttribute) == nodeTag)
+    val attrHandler = nodesKeys
+      .map(r => (r.getAs[String](idAttribute), GraphMLAttribute(r.getAs[String](nameAttribute), GraphMLTypes(r.getAs[String](typeAttribute)))))
+      .collect().toMap
 
-    val verticesWithData = graphDataFrame.flatMap(r => r.getAs[Any]("node") match {
-      case data: mutable.WrappedArray[Row @unchecked] => data.array
+    val verticesWithData = graphDataFrame.flatMap(r => r.getAs[Any](nodeTag) match {
+      case data: mutable.WrappedArray[Row@unchecked] => data.array
       case data: Row => Array(data)
     })
 
-    val verticesIndex = verticesWithData.map(r=>r.getAs[String]("@id")).zipWithUniqueId().collect().toMap
-    val vertices: RDD[(VertexId, Map[String,Any])] = verticesWithData
+    val verticesIndex = verticesWithData.map(r => r.getAs[String](idAttribute)).zipWithUniqueId().collect().toMap
+
+    val vertices: RDD[(VertexId, Map[String, Any])] = verticesWithData
       .map(
-        r=>(verticesIndex(r.getAs[String]("@id")),Try(r.getAs[mutable.WrappedArray[Row]]("data")).toOption.map(
-        _.map(r=>{
-          val attribute=attrHandler(r.getAs[String]("@key"))
-          (attribute.name,attribute.handler(r.getAs[String]("#VALUE")))
-        }).toMap
+        r => (verticesIndex(r.getAs[String](idAttribute)), Try(r.getAs[mutable.WrappedArray[Row]](dataTag)).toOption.map(
+          _.map(r => {
+            val attribute = attrHandler(r.getAs[String](keyAttribute))
+            (attribute.name, attribute.handler(r.getAs[String](tagValue)))
+          }).toMap
         ).getOrElse(Map.empty))
       )
-    vertices.collect()
-    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 vertices")
-    val edgesRows = graphDataFrame.flatMap(r => r.getAs[Any]("edge") match {
-      case data: mutable.WrappedArray[Row @unchecked] => data.array
+
+    val edgesRows = graphDataFrame.flatMap(r => r.getAs[Any](edgeTag) match {
+      case data: mutable.WrappedArray[Row@unchecked] => data.array
       case data: Row => Array(data)
     })
-      .map(r => Edge(verticesIndex(r.getAs[String]("@source")), verticesIndex(r.getAs[String]("@target")), Map[String,Any]("id"->r.getAs[String]("@id"))))
-    edgesRows.collect()
-    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 edges")
+      .map(r => Edge(
+        verticesIndex(r.getAs[String](sourceAttribute)),
+        verticesIndex(r.getAs[String](targetAttribute)),
+        Map[String,Any]("id"->r.getAs[String](idAttribute))
+      ))
     Graph(vertices, edgesRows)
   }
-
-
 
 }
