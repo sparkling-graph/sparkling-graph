1c986f489cc658746757c10e828e055d6b6efba8
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-04-06 22:04:58 +0200

    Fixing PSCAN implementation

--- operators/src/main/scala/ml/sparkling/graph/operators/algorithms/pscan/PSCAN.scala
+++ operators/src/main/scala/ml/sparkling/graph/operators/algorithms/pscan/PSCAN.scala
@@ -21,52 +21,27 @@ object PSCAN {
   
 
   type ComponentID=Long
-  case class PSCANData(componentID: ComponentID,isActive:Boolean)
 
 
   def computeConnectedComponents[VD:ClassTag,ED:ClassTag](graph:Graph[VD,ED],epsilon:Double=0.1):Graph[ComponentID,ED]={
 
     val neighbours: Graph[NeighbourSet, ED] = NeighboursUtils.getWithNeighbours(graph,treatAsUndirected = true)
+
     val edgesWithSimilarity=neighbours.mapTriplets(edge=>{
       val sizeOfIntersection=intersectSize(edge.srcAttr,edge.dstAttr)
       val denominator = Math.sqrt(edge.srcAttr.size()*edge.dstAttr.size())
       sizeOfIntersection/denominator
     })
+
     val cutOffGraph=edgesWithSimilarity.filter[NeighbourSet, Double](
       preprocess=g=>g,
       epred=edge=>{
       edge.attr >= epsilon
     })
 
-    val startGraph=cutOffGraph.mapVertices{
-      case (vId,data)=>PSCANData(vId,true)
-    }
+    val componentsGraph=cutOffGraph.connectedComponents()
 
-     val graphWithComponents=startGraph.pregel[List[ComponentID]](List.empty[Long])(
-      vprog=(vId,data,messages)=>{
-       val out= Try(messages.min).map(newId=>{
-        if(newId<data.componentID)
-          PSCANData(newId,true)
-          else
-          PSCANData(data.componentID,false)
-        }
-        ).getOrElse(PSCANData(data.componentID,data.isActive))
-        out
-      },
-      sendMsg=(edge)=>{
-        if(edge.srcAttr.isActive){
-          Iterator((edge.dstId,List(edge.srcAttr.componentID)),(edge.srcId,List(edge.srcAttr.componentID)))
-          }
-        else
-          Iterator()
-      },
-      mergeMsg=(a,b)=>a:::b
-      )
-    val verticesWithComponents=graphWithComponents.mapVertices{
-      case (vId,data)=>data.componentID
-    }.vertices
-
-    graph.outerJoinVertices(verticesWithComponents)((vId,oldData,newData)=>newData.get)
+    graph.outerJoinVertices(componentsGraph.vertices)((vId,oldData,newData)=>newData.get)
   }
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.3")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.3" % "test")
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.0")
-addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.0.3")
+addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.0.3" % "test")
 addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.3")
 resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
 addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.4")
