cdcfc3e9d837577d35fe94320bee7aacdded9336
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-04-12 14:01:23 +0200

    Code layout fix

--- operators/src/main/scala/ml/sparkling/graph/operators/OperatorsDSL.scala
+++ operators/src/main/scala/ml/sparkling/graph/operators/OperatorsDSL.scala
@@ -18,25 +18,32 @@ import scala.reflect.ClassTag
 object OperatorsDSL {
 
   implicit class DSL[VD:ClassTag,ED:ClassTag](graph:Graph[VD,ED]){
-    def PSCAN(epsilon:Double=0.1):Graph[ComponentID,ED]={
+    def PSCAN(epsilon:Double=0.1):Graph[ComponentID,ED]=
       computeConnectedComponents(graph,epsilon)
-    }
 
-    def closeness(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=Closeness.compute(graph,vertexMeasureConfiguration)
+    def closeness(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      Closeness.compute(graph,vertexMeasureConfiguration)
 
-    def eigenvectorCentrality(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=EigenvectorCentrality.compute(graph,vertexMeasureConfiguration)
+    def eigenvectorCentrality(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      EigenvectorCentrality.compute(graph,vertexMeasureConfiguration)
 
-    def hits(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=Hits.compute(graph,vertexMeasureConfiguration)
+    def hits(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      Hits.compute(graph,vertexMeasureConfiguration)
 
-    def degree(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=Degree.compute(graph,vertexMeasureConfiguration)
+    def degree(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      Degree.compute(graph,vertexMeasureConfiguration)
 
-    def neighborhoodConnectivity(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=NeighborhoodConnectivity.compute(graph,vertexMeasureConfiguration)
+    def neighborhoodConnectivity(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      NeighborhoodConnectivity.compute(graph,vertexMeasureConfiguration)
 
-    def vertexEmbeddedness(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=VertexEmbeddedness.compute(graph,vertexMeasureConfiguration)
+    def vertexEmbeddedness(vertexMeasureConfiguration: VertexMeasureConfiguration[VD, ED])(implicit num:Numeric[ED])=
+      VertexEmbeddedness.compute(graph,vertexMeasureConfiguration)
 
-    def partitionBy(communityDetectionMethod:CommunityDetectionMethod[VD,ED])(implicit sc:SparkContext)=partitionGraphBy(graph,communityDetectionMethod)
+    def partitionBy(communityDetectionMethod:CommunityDetectionMethod[VD,ED])(implicit sc:SparkContext)=
+      partitionGraphBy(graph,communityDetectionMethod)
 
-    def partitionBy(communityDetectionMethod:CommunityDetectionAlgorithm)(implicit sc:SparkContext)=partitionGraphBy(graph,communityDetectionMethod)
+    def partitionBy(communityDetectionMethod:CommunityDetectionAlgorithm)(implicit sc:SparkContext)=
+      partitionGraphBy(graph,communityDetectionMethod)
 
   }
 }
